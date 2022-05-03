19e8e89fffff6a71a87a2193f6a58e0bb98be895
Jérôme Dumas <jerome.dumas@yahoo.fr>
2022-05-03 19:05:27 +0200

    fix: fix wrong path in require.js dependencies

--- SPA/app/main.js
+++ SPA/app/main.js
@@ -1,3 +1,3 @@
-define(['require', 'SPA/lib/cards', 'timer'], function(require, cardsModule, timerModule) {
+define(['require', 'cards', 'timer'], function(require, cardsModule, timerModule) {
     cardsModule.init(); // On démarre le jeu.
 });
