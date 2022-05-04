ff1764386a43d641b7ba712cbd4a52ecf6b403a2
Jérôme Dumas <jerome.dumas@yahoo.fr>
2022-05-04 20:58:35 +0200

    feat: add basic api key authentication

---
+++ API/src/Security/PublicApiUser.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class PublicApiUser implements UserInterface
+{
+    public function __construct(private string $apiKey) {}
+
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->apiKey;
+    }
+
+}
